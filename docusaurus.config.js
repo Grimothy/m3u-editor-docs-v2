e2f58a04bc5acdbfb4214efcaee87ef3dae5c02a
Shaun Parkison <shparkison@gmail.com>
2026-01-06 14:20:20 -0700

    chore: Support light mode! Add mode toggle to allow user to toggle dark/light mode
    Replace hard-coded CSS values with the color variables for dark/light mode support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
       image: 'img/docusaurus-social-card.jpg',
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: true,
+        disableSwitch: false,
         respectPrefersColorScheme: false,
       },
       navbar: {
